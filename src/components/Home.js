8793349538c49c8db7fb1e1c2975fe1d092850f7
fajar241101 <mf.fajarramadhan.sh@gmail.com>
2023-03-23 11:15:57 +0700

    belajar react routing dom

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <p>Home Page</p>
+    </div>
+  )
+}
+
+export default Home;
