4fb7d6d058810ab09a3d31b23585b6e12642eca9
fajar241101 <mf.fajarramadhan.sh@gmail.com>
2023-03-23 11:21:36 +0700

    belajar react routing dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import Login from './components/Login';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
